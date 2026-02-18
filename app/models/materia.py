d21683a6ef89ba5f8bb265329d9eb47c7cf9eff5
ElielUT <elielpa.ti24@utsjr.edu.mx>
2026-02-18 07:37:03 -0600

    Otra corrección

--- app/models/materia.py
+++ app/models/materia.py
@@ -1,15 +1,12 @@
 from sqlalchemy import Column, Integer, String, ForeignKey
 from app.core.supabase_client import Base # Usamos la base que ya tienen en centro
+from pydantic import BaseModel
 
-class Materia(Base):
-    __tablename__ = "materia"
-    id_materia = Column(Integer, primary_key=True, index=True)
-    nombre = Column(String)
-    carrera = Column(String)
-    cuatrimestre = Column(Integer)
+class MateriaBase(BaseModel):
+    nombre: str
+    carrera: str
+    cuatrimestre: int
 
-class Impartir(Base):
-    __tablename__ = "impartir"
-    id_impartir = Column(Integer, primary_key=True, index=True)
-    id_materia2 = Column(Integer, ForeignKey("materia.id_materia"))
-    id_asesor2 = Column(Integer, ForeignKey("asesor.id_asesor"))
+class ImpartirBase(BaseModel):
+    id_materia2: int
+    id_asesor2: int
